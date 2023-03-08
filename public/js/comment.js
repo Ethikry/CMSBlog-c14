d07947156171c9129fa98b2c91cb036541b333d7
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-03-08 08:49:20 -0700

    Bugfixing comments and display

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,6 +3,7 @@ async function commentFormHandler(event) {
 
   const commentTextEl = document.querySelector('input[name="comment-body"]');
   const commentText = commentTextEl.value.trim();
+  console.log(commentText);
 
   if (!commentText) {
     return;
@@ -20,14 +21,13 @@ async function commentFormHandler(event) {
     if (!response.ok) {
       throw new Error(`Failed to create comment: ${response.statusText}`);
     }
-
+    console.log(commentText);
     document.location.reload();
   } catch (err) {
     alert(err.message);
     commentTextEl.focus();
   }
 }
-
 document
-  .querySelector(".comment-form")
+  .querySelector("#comment-form")
   .addEventListener("submit", commentFormHandler);
